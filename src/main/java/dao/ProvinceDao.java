31c89c5d06c455bc422916e0ccc80b75fb76fbbb
jiangxin <2379139689@qq.com>
2021-11-15 23:17:39 +0800

    Initial commit first 2021-11-15

---
+++ src/main/java/dao/ProvinceDao.java
@@ -0,0 +1,7 @@
+package dao;
+import domain.Province;
+
+import java.util.List;
+public interface ProvinceDao {
+    public List<Province> findAll();
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>省份选择</h2>
+</body>
+</html>
